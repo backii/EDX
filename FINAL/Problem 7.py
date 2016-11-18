187c7c82728d8e61e02f30c0a732619a3a037b2a
backii <michal.jakubski@gmail.com>
2016-11-18 16:47:46 +0100

    FINAL#2

--- FINAL/Problem 7.py
+++ FINAL/Problem 7.py
@@ -4,12 +4,12 @@ def general_poly (L):
     """ L, a list of numbers (n0, n1, n2, ... nk)
     Returns a function, which when applied to a value x, returns the value
     n0 * x^k + n1 * x^(k-1) + ... nk * x^0 """
-    if len(L) == 0:
-        return 0
-    elif len(L) == 1:
-        return L[0]
-    else:
-        return sum(L[len(L)-num-1]*10**num for num in range(len(L)))
+
+    def evaluate(x):
+        return sum(L[len(L)-num-1]*x**num for num in range(len(L)))
+
+    return evaluate
 
 
-print(general_poly([1, 2, 3, 4]))
+
+print(general_poly([1, 2, 3, 4])(1))
